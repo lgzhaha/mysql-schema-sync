4906b0ba7e11b26b4792523f55c22ba330387627
liuguoju <lgzhaha@msn.cn>
2023-05-22 11:29:11 +0800

    json输出

--- internal/alter.go
+++ internal/alter.go
@@ -63,7 +63,8 @@ func (ta *TableAlterData) String() string {
 		ta.Type,
 		strings.Join(relationTables, ","),
 		strings.TrimSpace(ta.Comment),
-		strings.Join(ta.SQL, "\n"),
+// 		strings.Join(ta.SQL, "\n"),
+		strings.Join(ta.SQL, ""),
 	)
 	return strings.TrimSpace(str)
 }
